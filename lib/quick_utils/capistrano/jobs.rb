8ff4dbbf58ace25510e2b67c98fc25a2aaca66e5
Alan Graham <alan@productlab.com>
2013-03-30 16:38:27 -0400

    fixing namespace issue

--- lib/quick_utils/capistrano/jobs.rb
+++ lib/quick_utils/capistrano/jobs.rb
@@ -1,16 +1,12 @@
 Capistrano::Configuration.instance.load do
 
-  namespace :deploy do
-    task :restart, :roles => :app, :except => { :no_release => true } do
-      jobs.restart
-    end
-  end
-
   namespace :jobs do
     desc "Restart job processes"
     task :restart, :roles => :app, :except => {:no_release => true} do
       run "cd #{current_path} && bundle exec script/job_processor -e #{deploy_env} restart"
     end
   end
+
+  after "deploy:restart", "jobs:restart"
 
 end
